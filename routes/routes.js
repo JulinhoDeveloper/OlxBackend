496fc55215a28d61a2a24c4f4d563333f74a05f8
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-10-09 13:44:18 -0300

    alterando as rotas

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const AuthController = require('./controllers/AuthController');
-const UserController = require('./controllers/UserController');
-const AdsController = require('./controllers/AdsController')
+const AuthController = require('../controllers/AuthController');
+const UserController = require('../controllers/UserController');
+const AdsController = require('../controllers/AdsController');
 
 router.get('/ping', (req,res)=>{
     res.json({pong: true});
@@ -13,7 +13,7 @@ router.get('/ping', (req,res)=>{
 router.get('/states', UserController.getStates);
 
 router.post('/user/signin', AuthController.signin);
-router.post('/user/signup', AuthController.signin);
+router.post('/user/signup', AuthController.signup);
 
 router.get('/user/me', UserController.info);
 router.put('/user/me', UserController.editAction);
