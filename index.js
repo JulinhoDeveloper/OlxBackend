281a049ffbd0e46e6d6fcf8f5ef1667faf26809d
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-10-10 20:24:33 -0300

    Validações do signup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+
 const cors = require('cors');
 const fileupload = require('express-fileupload');
 const { dbConnection } = require('./database/config');
